cd3c7fa8f761fcdc90277fa99a09e4fc222dfceb
52man <hagberg.kristian@gmail.com>
2017-11-06 13:28:37 +0100

    Add files via upload

--- 202.sql
+++ 202.sql
@@ -1,12 +1,20 @@
 
 use IS_202;
  
-CREATE TABLE contacts (
-  contact_id int(11) NOT NULL AUTO_INCREMENT,
-  first_name varchar(45) DEFAULT NULL,
-  last_name varchar(45) DEFAULT NULL,
-  file mediumblob,
-  PRIMARY KEY (contact_id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+Create table delivery (
+	Delivery_ID int not null AUTO_INCREMENT,
+	Comment varchar(200),
+	File mediumblob,
+	constraint delivery_PK primary key (Delivery_ID)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-select * from contacts;
+select * from delivery;
+drop table delivery;
+
+Create table Module (
+	Module_ID int not null AUTO_INCREMENT,
+	Title varchar(50) not null,
+	Description varchar(1000),
+	Delivery_date date,
+	constraint Module_PK primary key (Module_ID)
+);
