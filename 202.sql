fd886ce0ce3e3efdf3f421afe989eff7a9378f63
52man <hagberg.kristian@gmail.com>
2017-11-13 12:02:00 +0100

    Add files via upload

--- 202.sql
+++ 202.sql
@@ -3,9 +3,14 @@ use IS_202;
  
 Create table delivery (
 	Delivery_ID int not null AUTO_INCREMENT,
+    #Module_ID int not null,
+	#email varchar(255) not null,
 	Comment varchar(200),
 	File mediumblob,
+    approved boolean,
 	constraint delivery_PK primary key (Delivery_ID)
+    #foreign key (Module_ID) references Module(Module_ID),
+	#foreign key (email) references bruker(email)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 select * from delivery;
@@ -18,3 +23,5 @@ Create table Module (
 	Delivery_date date,
 	constraint Module_PK primary key (Module_ID)
 );
+
+select * from bruker;
