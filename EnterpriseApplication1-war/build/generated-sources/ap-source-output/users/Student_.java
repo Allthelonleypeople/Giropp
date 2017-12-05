d112e67cee1c61e8ea5c8eaec557e7472a8ea1dc
StianSim <stiansimonsen97@hotmail.com>
2017-12-05 10:50:22 +0100

    Revert "Add files via upload"
    This reverts commit c502c5ae193223b092c3afe2e1eab40e4b07116d.

--- EnterpriseApplication1-war/build/generated-sources/ap-source-output/users/Student_.java
+++ EnterpriseApplication1-war/build/generated-sources/ap-source-output/users/Student_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(Student.class)
-public abstract class Student_ extends users.Bruker_ {
+public abstract class Student_ extends users.User_ {
 
 
 }
